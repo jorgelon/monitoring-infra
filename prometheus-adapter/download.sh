3f8972b7b47a941a4dc0993a7f7f9e204bbfa1c5
Jorge Pérez Higuera <jorge.phiguera@bosonit.com>
2025-04-02 13:31:37 +0200

    first commit with prometheus adapter

---
+++ prometheus-adapter/download.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+echo "Showing release tags"
+curl -s "https://api.github.com/repos/kubernetes-sigs/prometheus-adapter/tags" | jq -r ".[].name"
+read -p "Tell me the release tag to be downloaded: " release
+
+echo "Cleaning ${release} directory"
+rm -rf ${release} ${release}-tmp
+
+echo "Cloning ${release}"
+git clone --branch ${release} --depth 1 https://github.com/kubernetes-sigs/prometheus-adapter.git ${release}-tmp
+mv ${release}-tmp/deploy/manifests ${release}
+
+echo "Cleaning up"
+rm -rf ${release}-tmp
+
+echo "Creating kustomization file from the yaml files inside the $release directory"
+cat <<EOF >${release}/kustomization.yaml
+resources:
+EOF
+
+find ${release}/ -type f -name "*.yaml" -not -name "kustomization.yaml" -printf "- %f\n" | sort >>${release}/kustomization.yaml
